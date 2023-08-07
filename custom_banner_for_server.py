585adb6f06605e291ba82c2c33e26b60840a9f94
Gabriel <gabrielkellersouza@gmail.com>
2023-08-07 14:47:47 -0300

    update code, add replace ' per  ' '

--- custom_banner_for_server.py
+++ custom_banner_for_server.py
@@ -4,8 +4,11 @@ import pyfiglet
 def display_banner(message):
     custom_fig = pyfiglet.Figlet(font='big')
     banner = custom_fig.renderText(message)
-    print(banner)
+    banner_without_quotes = banner.replace("'", ' ')
+    print(banner_without_quotes)
 
 
-server_name = "Your Banner"
+server_name = "Your Server"
+print("echo '")
 display_banner(f"{server_name}")
+print("'")
